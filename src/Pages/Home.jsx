c9139a146b54503150f37aad9b20737d1f468d43
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2025-03-01 19:00:35 +0530

    useEffect Click count

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import Header from '../Components/HeaderContent/Header'
 import Body from '../Components/BodyContent/Body'
 import './Home.css'
@@ -6,22 +6,29 @@ import './Home.css'
 
 function Home() {
   
+  // buttons logic increment and decrement
   const [count,setcount]= useState(0)
-
+  const [click, setclick] = useState(0)
   let increment = () => {setcount(count + 1)};
   let decrement = () => {setcount(count - 1)};
+  let clickCount = () => {setclick(click +1)};
 
 
+  // useeffect
+  useEffect(() =>{ console.log("hey") },[count])
+
+  
   return (
     <div>
       <Header />
       <Body>
         <h1>My HomePage </h1>
         <p>Count {count}</p>
-
         <button onClick={decrement} className="button">-</button>
         <button onClick={increment} className="button">+</button>
         
+        <p>You have clicked {click} times.</p>
+        <button onClick={clickCount} className="button">Click Me</button>
         
        
       </Body>
